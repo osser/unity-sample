d11454ccd83cf3a462d4352a753d1e540e5f2a68
Koma <admin@osser.jp>
2016-06-15 09:19:39 +0900

    LearnUnity: Scene03砲撃素材更新

---
+++ LearnUnity/Assets/Scripts/ScreenCursor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenCursor : MonoBehaviour
+{
+
+	// For GUI
+	public Texture CursorTexture;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void OnGUI ()
+	{
+		GUILayout.TextArea("Screen.width:" + Screen.width.ToString() + ", Screen.height:" + Screen.height.ToString());
+		GUILayout.TextArea(Input.mousePosition.ToString());
+
+		float left = Input.mousePosition.x - CursorTexture.width / 2;
+		float top = Screen.height - Input.mousePosition.y - CursorTexture.height / 2;
+		GUI.DrawTexture (new Rect (
+			left, 
+			top, 
+			CursorTexture.width, 
+			CursorTexture.height), CursorTexture);
+	}
+}
