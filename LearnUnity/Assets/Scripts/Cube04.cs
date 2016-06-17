d19aa7f147ca5cf58de8af1c69d69051c04be018
Koma <admin@osser.jp>
2016-06-17 16:18:28 +0900

    LearnUnity更新

---
+++ LearnUnity/Assets/Scripts/Cube04.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube04 : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//Mathf.Lerp(min,max,step);
+		float x = Mathf.Lerp(0, 5, Time.time);
+		Debug.Log("x:" + x);
+		this.transform.position = new Vector3 (
+			x,
+			transform.position.y,
+			transform.position.z
+		);
+	}
+}
---
+++ LearnUnity/Assets/Scripts/Init_Scene09.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Init_Scene09 : MonoBehaviour
+{
+
+	private Quaternion[] rotationValues;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rotationValues = new Quaternion[55];
+
+		for (var i = 0; i < rotationValues.Length; i++) {
+			GameObject cylinder = GameObject.CreatePrimitive (PrimitiveType.Cylinder);
+			cylinder.transform.localScale = new Vector3 (0.3f, 3, 0.3f);
+			cylinder.transform.position = new Vector3 (0, 3, 0);
+
+
+			//rotationValues [i] = Random.rotation;
+			rotationValues [i] = Random.rotationUniform;
+			cylinder.transform.rotation = rotationValues[i];
+			cylinder.GetComponent<MeshRenderer>().material.color = new Color(Random.value, Random.value, Random.value);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnGUI(){
+		GUILayout.TextArea("随机生成圆柱体，并旋转角度");
+	}
+}
