5ddc0a8b5f9c36b16c30bc48f71c5630b2fa3009
Koma <admin@osser.jp>
2016-06-22 10:47:50 +0900

    NejikoRun:障害物を追加

---
+++ NejikoRun/Assets/Scripts/EnemyPoint.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyPoint : MonoBehaviour
+{
+	public GameObject prefab;
+
+	// Use this for initialization
+	void Start ()
+	{
+		GameObject go = Instantiate (prefab, Vector3.zero, Quaternion.identity) as GameObject;
+
+		go.transform.SetParent (transform, false);
+	}
+
+	void OnDrawGizmos ()
+	{
+		Vector3 offset = new Vector3 (0, 0.5f, 0);
+
+		Gizmos.color = new Color (1, 0, 0, 0.5f);
+		Gizmos.DrawSphere (transform.position + offset, 0.5f);
+
+		if (prefab != null)
+			Gizmos.DrawIcon (transform.position + offset, prefab.name, true);
+	}
+
+}
--- NejikoRun/Assets/Scripts/NejikoController.cs
+++ NejikoRun/Assets/Scripts/NejikoController.cs
@@ -6,18 +6,32 @@ public class NejikoController : MonoBehaviour
 	const int MinLane = -2;
 	const int MaxLane = 2;
 	const float LaneWidth = 1.0f;
+	const int DefaultLife = 3;
+	const float StunDuration = 0.5f;
 
 	CharacterController controller;
 	Animator animator;
 
 	Vector3 moveDirection = Vector3.zero;
 	int targetLane;
+	int life = DefaultLife;
+	float recoverTime = 0.0f;
 
 	public float gravity = 0;
 	public float speedZ = 0;
 	public float speedX = 0;
 	public float speedJump = 0;
 	public float accelerationZ = 0;
+
+	public int Life ()
+	{
+		return life;
+	}
+
+	public bool IsStan ()
+	{
+		return recoverTime > 0.0f || life <= 0;
+	}
 
 	// Use this for initialization
 	void Start ()
@@ -29,22 +43,7 @@ public class NejikoController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-//		if (controller.isGrounded) {
-//			if (Input.GetAxis ("Vertical") > 0.0f) {
-//				moveDirection.z = Input.GetAxis ("Vertical") * speedZ;
-//			} else {
-//				moveDirection.z = 0;
-//			}
-//
-//			this.transform.Rotate (0, Input.GetAxis ("Horizontal") * 3, 0);
-//
-//			if (Input.GetButton ("Jump")) {
-//				moveDirection.y = speedJump;
-//				animator.SetTrigger ("jump");
-//			}
-//		}
 
-		//?????
 		if (Input.GetKeyDown ("left"))
 			MoveToLeft ();
 		if (Input.GetKeyDown ("right"))
@@ -52,11 +51,17 @@ public class NejikoController : MonoBehaviour
 		if (Input.GetKeyDown ("space"))
 			Jump ();
 
-		float accelerateZ = moveDirection.z + (accelerationZ * Time.deltaTime);
-		moveDirection.z = Mathf.Clamp (accelerateZ, 0, speedZ);
+		if (IsStan ()) {
+			moveDirection.x = 0.0f;
+			moveDirection.y = 0.0f;
+			recoverTime -= Time.deltaTime;
+		} else {
+			float accelerateZ = moveDirection.z + (accelerationZ * Time.deltaTime);
+			moveDirection.z = Mathf.Clamp (accelerateZ, 0, speedZ);
 
-		float ratioX = (targetLane * LaneWidth - transform.position.x) / LaneWidth;
-		moveDirection.x = ratioX * speedX;
+			float ratioX = (targetLane * LaneWidth - transform.position.x) / LaneWidth;
+			moveDirection.x = ratioX * speedX;
+		}
 
 		moveDirection.y -= gravity * Time.deltaTime;
 
@@ -71,22 +76,43 @@ public class NejikoController : MonoBehaviour
 
 	public void MoveToLeft ()
 	{
+		if (IsStan ())
+			return;
 		if (controller.isGrounded && targetLane > MinLane)
 			targetLane--;
 	}
 
 	public void MoveToRight ()
 	{
+		if (IsStan ())
+			return;
 		if (controller.isGrounded && targetLane < MaxLane)
 			targetLane++;
 	}
 
 	public void Jump ()
 	{
+		if (IsStan ())
+			return;
 		if (controller.isGrounded) {
 			moveDirection.y = speedJump;
 
 			animator.SetTrigger ("jump");
+		}
+	}
+
+	void OnControllerColliderHit (ControllerColliderHit hit)
+	{
+		if (IsStan ())
+			return;
+
+		if (hit.gameObject.tag == "Robo") {
+			life--;
+			recoverTime = StunDuration;
+
+			animator.SetTrigger("damage");
+
+			Destroy(hit.gameObject);
 		}
 	}
 }
