edd705a47d6e52e25ff0c7a5ec1effff7aa13607
Koma <admin@osser.jp>
2016-06-22 22:34:43 +0900

    NejikoRun:Life追加

---
+++ NejikoRun/Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public NejikoController nejiko;
+	public Text scoreLabel;
+	public LifePanel lifePanel;
+
+	// Update is called once per frame
+	void Update () {
+		int score = CalScore();
+		scoreLabel.text = "Score: " + score + " m";
+
+		lifePanel.UpdateLife(nejiko.Life());
+	}
+
+	int CalScore(){
+		return (int)nejiko.transform.position.z;
+	}
+}
---
+++ NejikoRun/Assets/Scripts/LifePanel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LifePanel : MonoBehaviour
+{
+
+	public GameObject[] icons;
+
+	public void UpdateLife (int life)
+	{
+		for (int i = 0; i < icons.Length; i++) {
+			if (i < life)
+				icons [i].SetActive (true);
+			else
+				icons [i].SetActive (false);
+		}
+	}
+}
