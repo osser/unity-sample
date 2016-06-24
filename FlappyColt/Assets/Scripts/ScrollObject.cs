6e26f767ca72c0ddb4a6245e2f1d0fc1e43517e2
Koma <admin@osser.jp>
2016-06-24 16:51:16 +0900

    FlappyColt:作成

---
+++ FlappyColt/Assets/Scripts/ScrollObject.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollObject : MonoBehaviour
+{
+	
+	public float speed = 1.0f;
+	public float startPosition;
+	public float endPosition;
+
+	void Start ()
+	{
+	}
+
+	void ScrollEnd ()
+	{
+		transform.Translate (-1 * (endPosition - startPosition), 0, 0);
+
+		SendMessage ("OnScrollEnd", SendMessageOptions.DontRequireReceiver);
+	}
+
+	void Update ()
+	{
+		transform.Translate (-1 * speed * Time.deltaTime, 0, 0);
+
+		if (transform.position.x <= endPosition)
+			ScrollEnd ();
+	
+	}
+}
