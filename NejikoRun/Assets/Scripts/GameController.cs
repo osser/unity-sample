97aa19b85ba2eeb26348e95f29ea3f74ed4ad228
Koma <admin@osser.jp>
2016-06-24 09:57:14 +0900

    NejikoRun:完成

--- NejikoRun/Assets/Scripts/GameController.cs
+++ NejikoRun/Assets/Scripts/GameController.cs
@@ -1,22 +1,42 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
 
 	public NejikoController nejiko;
 	public Text scoreLabel;
 	public LifePanel lifePanel;
 
 	// Update is called once per frame
-	void Update () {
-		int score = CalScore();
+	void Update ()
+	{
+		int score = CalScore ();
 		scoreLabel.text = "Score: " + score + " m";
 
-		lifePanel.UpdateLife(nejiko.Life());
+		lifePanel.UpdateLife (nejiko.Life ());
+
+		if (nejiko.Life () <= 0) {
+			enabled = false;
+
+			if (PlayerPrefs.GetInt ("HighScore") < score) {
+				PlayerPrefs.SetInt ("HighScore", score);
+			}
+
+			Invoke ("ReturnToTitle", 2);
+		}
 	}
 
-	int CalScore(){
+	int CalScore ()
+	{
 		return (int)nejiko.transform.position.z;
+	}
+
+	void ReturnToTitle ()
+	{
+		//Application.LoadLevel ("Title");
+		SceneManager.LoadScene("Title");
 	}
 }
---
+++ NejikoRun/Assets/Scripts/TitleController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleController : MonoBehaviour
+{
+
+	public Text highScoreLabel;
+
+	// Use this for initialization
+	void Start ()
+	{
+		highScoreLabel.text = "High Score:" + PlayerPrefs.GetInt("HighScore") + "m";
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public void OnStartButtonClicked ()
+	{
+		//Application.LoadLevel("Main");
+		SceneManager.LoadScene ("Main");
+	}
+}
