9e3a0433648e093219324c8e4e0f2c189f766722
Koma <admin@osser.jp>
2016-07-04 17:41:35 +0900

    LearnUGUI:Tabbar作成

---
+++ LearnUGUI/Assets/Scripts/TabBar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TabBar : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public void Tabbar_onclick (bool flg)
+	{
+		Debug.Log ("Tabbar_onclick:" + flg);
+	}
+}
